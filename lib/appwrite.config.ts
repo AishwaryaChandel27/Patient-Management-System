2caa88fdc5aeb6f6ab88ae871f388373b5154f9d
AishwaryaChandel27 <aishwaryachandel794@gmail.com>
2024-08-28 16:53:54 +0530

    update

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,10 @@
+const {
+    Project_Id,
+    API_Key,
+    DataBase_id,
+    Patient_ID,
+    Doctor_ID,
+    Appointment_ID,
+    Next_Public_Bucket_ID:Bucket_id,
+    Next_Public_Endpoint:Endpoint
+} = process.env;
